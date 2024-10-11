04bfd1776440af47b61152a0a0fbc382336171dc
Nikolay Glazov <glazov_n_o@mail.ru>
2024-10-11 17:41:53 +0300

    Add requirements.txt

--- ch4-2.py
+++ ch4-2.py
@@ -3,9 +3,9 @@ import openai
 
 def init_api():
     with open(".env") as env:
-	    for line in env:
-			key, value = line.strip().split("=")
-			os.environ[key] = value
+        for line in env:
+            key, value = line.strip().split("=")
+            os.environ[key] = value
 
     openai.api_key = os.environ.get("API_KEY")
     openai.organization = os.environ.get("ORG_ID")
